a08708e53496a0804ae24ee10888e9f8bac97a3d
Senthil-2710 <senthilvj2001@gmail.com>
2026-03-16 11:46:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -109,11 +109,9 @@ while True:
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     
-    if current_time == "11:45" and last_triggered != current_time:
+    if current_time == "11:48" and last_triggered != current_time:
         last_triggered = current_time
         study_alarm()
     
     print(f"Checking time: {current_time}")
     time.sleep(55)
-
-when i will get msg
